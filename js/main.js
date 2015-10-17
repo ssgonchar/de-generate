4428d536f0a0ec69aa044c79f970e76c27b188ce
Sergey Gonchar <fingercrew2@gmail.com>
2015-10-17 16:40:26 +0300

    Need make karma test for Trevis

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+App = function(config) {
+  this.config = config;
 
+  var appTitle = 'De-generator';
+
+  this.generate = function(tag, classes, label)
+  {
+    var html = '';
+
+    html += '<';
+    html += tag;
+    html += ' class="'+ classes +'"';
+    html += '>';
+    html += label.replace(/%s/g, appTitle);
+    html += '</';
+    html += tag;
+    html += '>';
+
+    return html;
+  }
+
+  this.show = function( button )
+  {
+    $('h1').html('You will be happy with us. You should start be happy and  '+button);
+  }
+}
+
+config = {
+  'env' : 'dev',
+}
+
+var app = new App(config);
+
+var button = app.generate('button','btn btn-primary', 'This is %s !');
